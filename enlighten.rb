b56b5368dc28e64bb8b90a8d3eff791b1395b187
Angel Vanegas <angelman@gmail.com>
2015-07-05 10:17:22 -0400

    fixed text formatting

--- enlighten.rb
+++ enlighten.rb
@@ -40,9 +40,9 @@ module Enlightenment
  # method for introducing the game
   def intro_game
     puts <<-END
-    You awaken in a bright room full of fog and
-    as your eyes adjust, the fog lifts and you find yourself
-    wanting more from life.
+You awaken in a bright room full of fog and
+as your eyes adjust, the fog lifts and you find yourself
+wanting more from life.
 
     END
     if $run == 1
@@ -61,9 +61,9 @@ module Enlightenment
   # method for the mid point
   def end_game
     puts <<-END
-    You awaken in a bright room full of fog and
-    as your eyes adjust, the fog lifts and you find yourself
-    at a fork in the road.
+You awaken in a bright room full of fog and
+as your eyes adjust, the fog lifts and you find yourself
+at a fork in the road.
 
     END
     display_choices {"(M)editate, (C)ontemplate, (I)ntrospect"}
@@ -75,10 +75,10 @@ module Enlightenment
     case gets.chomp.upcase
     when "G"
       puts <<-END
-      You step through the door, go about your life as usual with
-      a nagging feeling in the back of your mind that there was something
-      you wanted but can no longer remember. Finally, at your death bed,
-      you remember. But by then...
+You step through the door, go about your life as usual with
+a nagging feeling in the back of your mind that there was something
+you wanted but can no longer remember. Finally, at your death bed,
+you remember. But by then...
 
       END
       sleep(2)
@@ -86,9 +86,9 @@ module Enlightenment
 
     when "S"
       puts <<-END
-      You decide staying in the room is much better than leaving the room.
-      But you quickly get bored and go through the door. Life happens. Then
-      you die.
+You decide staying in the room is much better than leaving the room.
+But you quickly get bored and go through the door. Life happens. Then
+you die.
 
       END
       sleep(2)
@@ -96,15 +96,15 @@ module Enlightenment
 
     when "I"
       puts <<-END
-      You close your eyes and focus on your breath.
-      As you breathe in...and out.... you travel to the planes
-      of the inner mind.
+You close your eyes and focus on your breath.
+As you breathe in...and out.... you travel to the planes
+of the inner mind.
 
       END
       sleep(2)
   else
     puts <<-END
-    You die from incompetence.
+You die from incompetence.
 
     END
     sleep(2)
@@ -117,41 +117,41 @@ end
     case gets.chomp.upcase
     when "G"
       puts <<-END
-      You feel as if you have done this before but shake the feeling away
-      as a simple case of deja vu. You face life just like you always have
-      perhaps achieve a modicum of fame and fortune, but in the end,
-      it didn't matter.
+You feel as if you have done this before but shake the feeling away
+as a simple case of deja vu. You face life just like you always have
+perhaps achieve a modicum of fame and fortune, but in the end,
+it didn't matter.
 
       END
       sleep(2)
       $death_count += 1
     when "S"
       puts <<-END
-      Staying in the room isn't so bad, after all,
-      you were in here for a reason.
-      You notice a shiny object in the corner and bend over to inspect it.
+Staying in the room isn't so bad, after all,
+you were in here for a reason.
+You notice a shiny object in the corner and bend over to inspect it.
       END
       sleep(2)
       display_choices {"(P)ick object up, (L)eave it be"}
       case gets.chomp.upcase
       when "P"
         puts <<-END
-        You decide to pick it up. It appears to be a ring.
+You decide to pick it up. It appears to be a ring.
 
         END
         sleep(2)
         $inventory.push("ring")
       when "L"
         puts <<-END
-        You don't want to mess with anything,
-        you realize nothing in the room looks familiar. Where are you?
+You don't want to mess with anything,
+you realize nothing in the room looks familiar. Where are you?
 
         END
         $inventory.push("map")
         sleep(2)
       else
         puts <<-END
-        You've done it again. You die TWICE for your silliness.
+You've done it again. You die TWICE for your silliness.
 
         END
         sleep(2)
@@ -160,9 +160,9 @@ end
       end
     when "I"
       puts <<-END
-      You open your eyes and wonder if what you are seeing is real. You also
-      can't seem to tell if you've ever died or not. Perhaps it's time to
-      go through the door.
+You open your eyes and wonder if what you are seeing is real. You also
+can't seem to tell if you've ever died or not. Perhaps it's time to
+go through the door.
 
       END
       sleep(2)
@@ -170,7 +170,7 @@ end
     else
       if $miss > 0
         puts <<-END
-        Surprisingly, you don't die. Instead, you get a skunk.
+Surprisingly, you don't die. Instead, you get a skunk.
 
         END
         sleep(2)
@@ -201,22 +201,22 @@ end
       case gets.chomp.upcase
       when "M"
         puts <<-END
-        You attempt to quiet your mind but it zips all over.
+You attempt to quiet your mind but it zips all over.
 
         END
       when "C"
         puts <<-END
-        Perhaps you should avoid dying so often.
+Perhaps you should avoid dying so often.
 
         END
       when "I"
         puts <<-END
-        You realize death has no meaning.
+You realize death has no meaning.
 
         END
       else
         puts <<-END
-        THIS IS JUST A GAME.
+THIS IS JUST A GAME.
 
         END
       end
@@ -231,21 +231,21 @@ end
       case gets.chomp.upcase
       when "M"
         puts <<-END
-        Your thoughts go to the ring you found previously, in, what?
-        another life? another timeline? Perhaps if we went deeper,
-        we'd find out more...
+Your thoughts go to the ring you found previously, in, what?
+another life? another timeline? Perhaps if we went deeper,
+we'd find out more...
 
         END
       when "C"
         puts <<-END
-        You think deeply about your options and realize they all mean the same
-        thing. Maybe this is a clue to the correct path?
+You think deeply about your options and realize they all mean the same
+thing. Maybe this is a clue to the correct path?
 
         END
       when "I"
         puts <<-END
-        You may find this ending unsatisfying, but the simplest truths usually
-        are.
+You may find this ending unsatisfying, but the simplest truths usually
+are.
 
         END
       else
@@ -255,29 +255,29 @@ end
       case gets.chomp.upcase
       when "M"
         puts <<-END
-        You fish around your pockets and find a map. A map to the mind!
-        Where shall you explore first? Traverse the plains of hilarity
-        or will you speak to the other beings the dwell, hidden from view?
-        Only you can decide!
+You fish around your pockets and find a map. A map to the mind!
+Where shall you explore first? Traverse the plains of hilarity
+or will you speak to the other beings the dwell, hidden from view?
+Only you can decide!
 
         END
       when "C"
         puts <<-END
-        You decide to embark on the biggest challenge of your life! This is,
-        unquestionably, the most difficult expedition you will embark on. The
-        end goal, to Be.
+You decide to embark on the biggest challenge of your life! This is,
+unquestionably, the most difficult expedition you will embark on. The
+end goal, to Be.
 
         END
       when "I"
         puts <<-END
-        You've mapped out your life, now it's time to map out your next lives.
-        All it takes is a leap.
+You've mapped out your life, now it's time to map out your next lives.
+All it takes is a leap.
 
         END
       else
         puts <<-END
-        A rebellious mind may work for now, but eventually you will
-        calm down. Wake up, now.
+A rebellious mind may work for now, but eventually you will
+calm down. Wake up, now.
 
         END
       end
@@ -285,28 +285,28 @@ end
       case gets.chomp.upcase
       when "M"
         puts <<-END
-        You have a skunk. A SKUNK! Why, why didn't you just do what was asked?
-        You and I know why. It's ok, though, I understand.
+You have a skunk. A SKUNK! Why, why didn't you just do what was asked?
+You and I know why. It's ok, though, I understand.
 
         END
       when "C"
         puts <<-END
-        What's funny is that you kept the skunk. You smell atrocious, but you,
-        yeah you, don't seem to care. Which is a good start.
+What's funny is that you kept the skunk. You smell atrocious, but you,
+yeah you, don't seem to care. Which is a good start.
 
         END
       when "I"
         puts <<-END
-        You take the skunk out of your pocket (how on earth did
-        it fit in there?!) and set it free. It thanks you by lifting its
-        tail and spraying you with a scent unmatched by any garbage dump.
-        But at least you didn't die.
+You take the skunk out of your pocket (how on earth did
+it fit in there?!) and set it free. It thanks you by lifting its
+tail and spraying you with a scent unmatched by any garbage dump.
+But at least you didn't die.
 
         END
       else
         puts <<-END
-        We just can't win with you, can we? You get a skunk and you still
-        defy us. No matter, see you in the next life.
+We just can't win with you, can we? You get a skunk and you still
+defy us. No matter, see you in the next life.
 
         END
       end
@@ -329,8 +329,8 @@ end
         puts "A clean life, a clean path. You have always known. Keep at it."
       else
         puts <<-END
-        A mistake is all that is needed to start your quest.
-        You slipped and died.
+A mistake is all that is needed to start your quest.
+You slipped and died.
 
         END
         $miss += 1
